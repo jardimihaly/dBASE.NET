e14a8bc856b299154516b98ee6a35800eee55a72
Járdi Mihály <jardi.m@outlook.com>
2022-01-19 08:33:59 +0100

    Fixed test failing caused by decimal separator inconsitencies between different cultures

--- dBASE.NET.Tests/DbfRecordTests.cs
+++ dBASE.NET.Tests/DbfRecordTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Aronic.dBASE.NET.Tests
@@ -9,8 +10,13 @@ namespace Aronic.dBASE.NET.Tests
         Dbf dbf;
 
         [TestInitialize]
-        public void testInit()
+        public void TestInit()
         {
+            // Fix decimal separator issue caused by OS culture
+            var enUSCulture = new System.Globalization.CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = enUSCulture;
+            Thread.CurrentThread.CurrentUICulture = enUSCulture;
+
             dbf = new Dbf();
             dbf.Read("fixtures/83/dbase_83.dbf");
         }
@@ -19,15 +25,15 @@ namespace Aronic.dBASE.NET.Tests
         public void ToStringTest()
         {
             Assert.AreEqual(
-                "ID=87,CATCOUNT=2,AGRPCOUNT=0,PGRPCOUNT=0,ORDER=87,CODE=1,NAME=Assorted Petits Fours,THUMBNAIL=graphics/00000001/t_1.jpg,IMAGE=graphics/00000001/1.jpg,PRICE=0,COST=0,DESC=,WEIGHT=5,51,TAXABLE=True,ACTIVE=True",
+                "ID=87,CATCOUNT=2,AGRPCOUNT=0,PGRPCOUNT=0,ORDER=87,CODE=1,NAME=Assorted Petits Fours,THUMBNAIL=graphics/00000001/t_1.jpg,IMAGE=graphics/00000001/1.jpg,PRICE=0,COST=0,DESC=,WEIGHT=5.51,TAXABLE=True,ACTIVE=True",
                 dbf.Records[0].ToString(),
                 "Default separator and mask");
             Assert.AreEqual(
-                "ID=87; CATCOUNT=2; AGRPCOUNT=0; PGRPCOUNT=0; ORDER=87; CODE=1; NAME=Assorted Petits Fours; THUMBNAIL=graphics/00000001/t_1.jpg; IMAGE=graphics/00000001/1.jpg; PRICE=0; COST=0; DESC=; WEIGHT=5,51; TAXABLE=True; ACTIVE=True",
+                "ID=87; CATCOUNT=2; AGRPCOUNT=0; PGRPCOUNT=0; ORDER=87; CODE=1; NAME=Assorted Petits Fours; THUMBNAIL=graphics/00000001/t_1.jpg; IMAGE=graphics/00000001/1.jpg; PRICE=0; COST=0; DESC=; WEIGHT=5.51; TAXABLE=True; ACTIVE=True",
                 dbf.Records[0].ToString("; "),
                 "Custom separator and default mask");
             Assert.AreEqual(
-                "ID->87; CATCOUNT->2; AGRPCOUNT->0; PGRPCOUNT->0; ORDER->87; CODE->1; NAME->Assorted Petits Fours; THUMBNAIL->graphics/00000001/t_1.jpg; IMAGE->graphics/00000001/1.jpg; PRICE->0; COST->0; DESC->; WEIGHT->5,51; TAXABLE->True; ACTIVE->True",
+                "ID->87; CATCOUNT->2; AGRPCOUNT->0; PGRPCOUNT->0; ORDER->87; CODE->1; NAME->Assorted Petits Fours; THUMBNAIL->graphics/00000001/t_1.jpg; IMAGE->graphics/00000001/1.jpg; PRICE->0; COST->0; DESC->; WEIGHT->5.51; TAXABLE->True; ACTIVE->True",
                 dbf.Records[0].ToString("; ", "{name}->{value}"),
                 "Custom separator and mask");
             Assert.AreEqual(
@@ -35,7 +41,7 @@ namespace Aronic.dBASE.NET.Tests
                 dbf.Records[0].ToString("; ", "{name}"),
                 "Custom separator and mask with only name");
             Assert.AreEqual(
-                "87; 2; 0; 0; 87; 1; Assorted Petits Fours; graphics/00000001/t_1.jpg; graphics/00000001/1.jpg; 0; 0; ; 5,51; True; True",
+                "87; 2; 0; 0; 87; 1; Assorted Petits Fours; graphics/00000001/t_1.jpg; graphics/00000001/1.jpg; 0; 0; ; 5.51; True; True",
                 dbf.Records[0].ToString("; ", "{value}"),
                 "Custom separator and mask with only value");
         }
